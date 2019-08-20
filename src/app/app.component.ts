e3a4b1b492e43826065104f0659e2b64e9a78dcc
PrimalIT <antoniocaiella97@gmail.com>
2019-08-20 14:33:34 +0200

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  actualFor;
+  actualDes;
+  actualCos;
+  actualSen;
+  actualSpi;
+  exp;
+
+  private calcoloExp(){
+    this.exp=this.actualFor;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {SelectItem} from 'primeng/api';
+
+import {ButtonModule} from 'primeng/button';
+import {AccordionModule} from 'primeng/accordion';
+import {MenuItem} from 'primeng/api';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, ButtonModule, AccordionModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+ }
